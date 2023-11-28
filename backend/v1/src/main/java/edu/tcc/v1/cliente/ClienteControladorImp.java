dd202c9bf5f4b3dd43453c78ac197fd918cc2bc9
lazarodamasceno <lazarolawrence2@gmail.com>
2023-11-28 00:26:59 -0300

    Implementou o controlador da entidade Cliente

--- backend/v1/src/main/java/edu/tcc/v1/cliente/ClienteControladorImp.java
+++ backend/v1/src/main/java/edu/tcc/v1/cliente/ClienteControladorImp.java
@@ -16,71 +16,71 @@ public class ClienteControladorImp implements ClienteControlador {
     private ClienteServicoImp servico;
 
     @Override
-    public ResponseEntity<Void> cadastrarCliente(CadastrarClienteDTO dto) {
-        return null;
+    public ResponseEntity<Void> cadastrarCliente(@RequestBody CadastrarClienteDTO dto) {
+        return servico.cadastrarCliente(dto);
     }
 
     @Override
     @PostMapping("cadastrar/consulta/{cpf}")
-    public ResponseEntity<Void> cadastrarConsulta(String cpf,
-                                                  AgendarConsultaDTO dto) {
-        return null;
+    public ResponseEntity<Void> cadastrarConsulta(@PathVariable String cpf,
+                                                  @RequestBody AgendarConsultaDTO dto) {
+        return servico.cadastrarConsulta(cpf, dto);
     }
 
     @Override
     @PatchMapping("cancelar/consulta/{cpf}/{dataAgendamento}")
-    public ResponseEntity<Void> cancelarConsulta(String cpf,
-                                                 String dataAgendamento) {
-        return null;
+    public ResponseEntity<Void> cancelarConsulta(@PathVariable String cpf,
+                                                 @PathVariable String dataAgendamento) {
+        return servico.cancelarConsulta(cpf, dataAgendamento);
     }
 
     @Override
     @GetMapping("exibir/consultas/{cpf}")
-    public ResponseEntity<List<Consulta>> exibirTodasAsConsultas(String cpf) {
-        return null;
+    public ResponseEntity<List<Consulta>> exibirTodasAsConsultas(@PathVariable String cpf) {
+        return servico.exibirTodasAsConsultas(cpf);
     }
 
     @Override
     @GetMapping("exibir/consultas/agendadas/{cpf}")
-    public ResponseEntity<List<Consulta>> exibirConsultasAgendadas(String cpf) {
-        return null;
+    public ResponseEntity<List<Consulta>> exibirConsultasAgendadas(@PathVariable String cpf) {
+        return servico.exibirConsultasAgendadas(cpf);
     }
 
     @Override
     @GetMapping("exibir/consultas/canceladas/{cpf}")
-    public ResponseEntity<List<Consulta>> exibirConsultasCanceladas(String cpf) {
-        return null;
+    public ResponseEntity<List<Consulta>> exibirConsultasCanceladas(@PathVariable String cpf) {
+        return servico.exibirConsultasCanceladas(cpf);
     }
 
     @Override
     @GetMapping("exibir/consultas/{cpf}/{dataInicial}/{dataFinal}")
-    public ResponseEntity<List<Consulta>> exibirConsultasEntreDatas(String cpf,
-                                                                    String dataInicial,
-                                                                    String dataFinal) {
-        return null;
+    public ResponseEntity<List<Consulta>> exibirConsultasEntreDatas(@PathVariable String cpf,
+                                                                    @PathVariable String dataInicial,
+                                                                    @PathVariable String dataFinal) {
+        return servico.exibirConsultasEntreDatas(cpf, dataInicial, dataFinal);
     }
 
     @Override
     @GetMapping("exibir/consultas/agendadas/{cpf}/{dataInicial}/{dataFinal}")
-    public ResponseEntity<List<Consulta>> exibirConsultasAgendadasEntreDatas(String cpf,
-                                                                             String dataInicial,
-                                                                             String dataFinal) {
-        return null;
+    public ResponseEntity<List<Consulta>> exibirConsultasAgendadasEntreDatas(@PathVariable String cpf,
+                                                                             @PathVariable String dataInicial,
+                                                                             @PathVariable String dataFinal) {
+        return servico.exibirConsultasAgendadasEntreDatas(cpf, dataInicial, dataFinal);
     }
 
     @Override
     @GetMapping("exibir/consultas/canceladas/{cpf}/{dataInicial}/{dataFinal}")
-    public ResponseEntity<List<Consulta>> exibirConsultasCanceladasEntreDatas(String cpf,
-                                                                              String dataInicial,
-                                                                              String dataFinal) {
-        return null;
+    public ResponseEntity<List<Consulta>> exibirConsultasCanceladasEntreDatas(@PathVariable String cpf,
+                                                                              @PathVariable String dataInicial,
+                                                                              @PathVariable String dataFinal) {
+        return servico.exibirConsultasCanceladasEntreDatas(cpf, dataInicial, dataFinal);
     }
 
     @Override
     @GetMapping("exibir/consultas/{cpf}/{nomeMedico}")
-    public ResponseEntity<List<Consulta>> exibirConsultasPeloNomeDoMedico(String cpf,
-                                                                          String nomeMedico) {
-        return null;
+    public ResponseEntity<List<Consulta>> exibirConsultasPeloNomeDoMedico(@PathVariable String cpf,
+                                                                          @PathVariable String nomeMedico) {
+        return servico.exibirConsultasPeloNomeDoMedico(cpf, nomeMedico);
     }
 
 }
