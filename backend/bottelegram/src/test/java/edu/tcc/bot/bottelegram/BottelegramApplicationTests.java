e51a9998b4b3794af232238e692ff9bac332b61d
lazarodamasceno <lazarolawrence2@gmail.com>
2023-12-12 00:01:11 -0300

    Separou o bot do telegram rpoegram da api principal

---
+++ backend/bottelegram/src/test/java/edu/tcc/bot/bottelegram/BottelegramApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.tcc.bot.bottelegram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BottelegramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
