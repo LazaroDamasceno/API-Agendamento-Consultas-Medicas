9aaccc342c8e2323e2128ec2465d9b5fc19b85e2
lazarodamasceno <lazarolawrence2@gmail.com>
2023-12-16 00:12:55 -0300

    Tornou a entidade Prontuário pública ao invés de visível apenas ao pacote no qual ela pertence

--- backend/api/v1/src/main/java/edu/tcc/v1/prontuario/Prontuario.java
+++ backend/api/v1/src/main/java/edu/tcc/v1/prontuario/Prontuario.java
@@ -20,7 +20,7 @@ import java.util.UUID;
 @Getter
 @Setter
 @NoArgsConstructor
-class Prontuario  implements Serializable {
+public class Prontuario  implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
