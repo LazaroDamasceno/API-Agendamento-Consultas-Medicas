c3e5b2575bf043d2aea179c4fb8551c987e98da7
Lázaro Lawrence Pereira Damasceno <62726118+LazaroDamasceno@users.noreply.github.com>
2024-01-10 16:43:57 -0300

    Update Cliente.java

--- backend/api/v1/src/main/java/edu/tcc/v1/cliente/Cliente.java
+++ backend/api/v1/src/main/java/edu/tcc/v1/cliente/Cliente.java
@@ -20,7 +20,6 @@ import java.util.UUID;
 @NoArgsConstructor
 public class Cliente implements Serializable {
 
-    @Serial
     private static final long serialVersionUID = 1L;
 
     @GeneratedValue(strategy = GenerationType.AUTO)
