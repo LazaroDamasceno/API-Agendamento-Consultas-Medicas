569448b1d9063345d5e42a1f1a19283056870e8f
lazarodamasceno <lazarolawrence2@gmail.com>
2024-01-19 22:26:35 -0300

    Fez pequenas alterações

--- backend/api/v1/src/main/java/edu/tcc/v1/prontuario/Prontuario.java
+++ backend/api/v1/src/main/java/edu/tcc/v1/prontuario/Prontuario.java
@@ -8,7 +8,6 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import java.io.Serial;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
